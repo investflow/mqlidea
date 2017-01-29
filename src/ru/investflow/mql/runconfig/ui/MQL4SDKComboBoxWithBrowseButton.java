bfe35579fa65862812bb225655d8e9fd8ea37a8d
Mikhail Fursov <mike.fursov@gmail.com>
2017-01-29 15:30:56 -0500

    Fix for WebStorm failure on adding new MQL4 SDK

--- src/ru/investflow/mql/runconfig/ui/MQL4SDKComboBoxWithBrowseButton.java
+++ src/ru/investflow/mql/runconfig/ui/MQL4SDKComboBoxWithBrowseButton.java
@@ -4,9 +4,8 @@ import com.intellij.openapi.application.Result;
 import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.ProjectJdkTable;
+import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.impl.SdkConfigurationUtil;
-import com.intellij.openapi.roots.ui.configuration.ProjectStructureConfigurable;
-import com.intellij.openapi.roots.ui.configuration.projectRoot.ProjectSdksModel;
 import com.intellij.ui.ComboboxWithBrowseButton;
 import org.jetbrains.annotations.NotNull;
 import ru.investflow.mql.sdk.MQL4SdkType;
@@ -23,17 +22,18 @@ public class MQL4SDKComboBoxWithBrowseButton extends ComboboxWithBrowseButton {
         this.comboBox = (MQL4SDKComboBox) getComboBox();
 
         addActionListener(e -> SdkConfigurationUtil.selectSdkHome(MQL4SdkType.INSTANCE, sdkHome -> {
-            ProjectSdksModel m = ProjectStructureConfigurable.getInstance(project).getProjectJdksModel();
-            m.addSdk(MQL4SdkType.INSTANCE, sdkHome, sdk -> {
-                new WriteAction<Void>() {
-                    @Override
-                    protected void run(@NotNull Result result) throws Throwable {
-                        ProjectJdkTable.getInstance().addJdk(sdk);
-                    }
-                }.execute();
-                comboBox.addItem(sdk);
-                comboBox.setSelectedSdk(sdk);
-            });
+            Sdk sdk = SdkConfigurationUtil.createAndAddSDK(sdkHome, MQL4SdkType.INSTANCE);
+            if (sdk == null) {
+                return;
+            }
+            new WriteAction<Void>() {
+                @Override
+                protected void run(@NotNull Result result) throws Throwable {
+                    ProjectJdkTable.getInstance().addJdk(sdk);
+                }
+            }.execute();
+            comboBox.addItem(sdk);
+            comboBox.setSelectedSdk(sdk);
         }));
     }
 }
