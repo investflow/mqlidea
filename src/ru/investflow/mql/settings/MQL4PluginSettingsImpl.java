adb6e9dc0d07aa1111578f2762a502a537836cf2
Mikhail Fursov <mike.fursov@gmail.com>
2020-04-12 13:00:20 -0400

    Removing deprecated and not needed anymore ApplicationComponent

--- src/ru/investflow/mql/settings/MQL4PluginSettingsImpl.java
+++ src/ru/investflow/mql/settings/MQL4PluginSettingsImpl.java
@@ -1,6 +1,5 @@
 package ru.investflow.mql.settings;
 
-import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
@@ -9,7 +8,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @State(name = "MQL4.PluginSettings", storages = {@Storage("mql4-plugin.xml")})
-public class MQL4PluginSettingsImpl implements MQL4PluginSettings, PersistentStateComponent<MQL4PluginSettingsImpl>, ApplicationComponent {
+public class MQL4PluginSettingsImpl implements MQL4PluginSettings, PersistentStateComponent<MQL4PluginSettingsImpl> {
 
     public boolean enDocs;
     public boolean errorAnalysis = true;
@@ -34,22 +33,6 @@ public class MQL4PluginSettingsImpl implements MQL4PluginSettings, PersistentSta
         errorAnalysis = v;
     }
 
-    @Override
-    public void initComponent() {
-
-    }
-
-    @Override
-    public void disposeComponent() {
-
-    }
-
-    @NotNull
-    @Override
-    public String getComponentName() {
-        return "MQL4.PluginSettings";
-    }
-
     @Nullable
     @Override
     public MQL4PluginSettingsImpl getState() {
@@ -57,7 +40,7 @@ public class MQL4PluginSettingsImpl implements MQL4PluginSettings, PersistentSta
     }
 
     @Override
-    public void loadState(MQL4PluginSettingsImpl state) {
+    public void loadState(@NotNull MQL4PluginSettingsImpl state) {
         XmlSerializerUtil.copyBean(state, this);
     }
 }
