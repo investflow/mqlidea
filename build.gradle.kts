f51e89958b2a38b08637b388c0a3a89445cc8211
lachlan <lachlan.odonnell@limemojito.com>
2024-02-26 16:50:22 +1100

    Moved resources.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.2.5")
+    version.set("2023.3.4")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
